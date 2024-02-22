c0d7db85574c396a925812aa23b56b93800d4e67
jordanmr1999 <jordanramkalawan@gmail.com>
2024-02-21 22:04:46 -0400

    navbar & LandingPage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing_Page from './Components/Landing_Page/Landing_Page';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+          <Navbar/>
+              <Routes>
+                <Route path="/" element={<Landing_Page/>}/>
+              </Routes>
+            
+        </BrowserRouter>
+       
     </div>
   );
-}
 
+}
 export default App;
